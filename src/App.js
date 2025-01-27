aa73cb6e9a6918167cbb537f201de8af8efd2ff7
Chandana Kovelamudi <77269607+Chandana472@users.noreply.github.com>
2025-01-27 01:15:06 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.python.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
